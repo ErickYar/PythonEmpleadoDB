a3c36dca3915a0e300938e65da72f93c459d2ccd
ErickYar <erickyb1985@gmail.com>
2023-12-06 13:55:52 -0500

    archivo models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+# Crear la clase empleado
+
+class Empleado(models.Model):
+    #definir atributos
+    nombre=models.CharField(max_length=30)
+    apellidos=models.CharField(max_length=30)
+    email=models.EmailField(max_length=60)
+    sexo=models.CharField(max_length=15,default='Masculino')
+    edad=models.IntegerField()
+    fecha=models.DateField()
+    sueldo=models.FloatField()
+
+    class Meta:
+        db_table='Empleado'
+
+    def __str__(self):
+        return self.nombre+' '+self.apellidos
+
+
